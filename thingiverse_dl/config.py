17f9f27a71b9ff2f2c1c03e3309b9812a5cde00f
Doug McGeehan <djmvfb@mst.edu>
2020-04-12 10:42:14 -0600

    some more refactoring

--- thingiverse_dl/config.py
+++ thingiverse_dl/config.py
@@ -15,32 +15,37 @@ class defaults(object):
 
 
 class secrets(object):
-    yml_file = pathlib.Path(__file__).parent.parent.resolve() / 'secrets.yml'
-    # yml_dict = yaml.load(yml_file.read_text(),
-    #                      Loader=yaml.BaseLoader)
-    expected_config_keys = {'client_id', 'client_secret', 'app_token'}
+    _yml_file = pathlib.Path(__file__).parent.parent.resolve() / 'secrets.yml'
+    client_id = None
+    client_secret = None
+    app_token = None
 
 
 # load secrets.yml file and assign values to the secrets config object
 try:
-    yaml_dict = yaml.load(secrets.yml_file.read_text(),
+    yaml_dict = yaml.load(secrets._yml_file.read_text(),
                           Loader=yaml.BaseLoader)
-    missing_config_keys = secrets.expected_config_keys.difference(yaml_dict.keys())
+    expected_secret_keys = set(filter(lambda k: not k.startswith('_'),
+                                      dir(secrets)))
+    missing_config_keys = expected_secret_keys.difference(yaml_dict.keys())
     if len(missing_config_keys) > 0:
         raise KeyError()
+
 except FileNotFoundError as e:
     # User did not create the `secrets.yml` file
-    logger.critical(f'Thingiverse secrets file not found at {secrets.yml_file}.\n'
+    logger.critical(f'Thingiverse secrets file not found at {secrets._yml_file}.\n'
                     f'Please register your usage of this app with Thingiverse, '
-                    f'populate the "{secrets.yml_file.name}.template" file, '
-                    f'and rename it to "{secrets.yml_file.name}"')
+                    f'populate the "{secrets._yml_file.name}.template" file, '
+                    f'and rename it to "{secrets._yml_file.name}"')
     sys.exit(1)
+
 except KeyError as e:
     # User did not properly format the `secrets.yml` file
-    logger.critical(f'{secrets.yml_file} is incomplete.\n'
-                    f'\tExpected keys: {", ".join(secrets.expected_config_keys)}\n'
+    logger.critical(f'{secrets._yml_file} is incomplete.\n'
+                    f'\tExpected keys: {", ".join(expected_secret_keys)}\n'
                     f'\tMissing keys:  {", ".join(missing_config_keys)}')
     sys.exit(1)
+
 else:
-    for key in secrets.expected_config_keys:
+    for key in expected_secret_keys:
         setattr(secrets, key, yaml_dict[key])
