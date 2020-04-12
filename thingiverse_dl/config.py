317f2bbe278f3929319773c6d2c19f7fba2babda
Doug McGeehan <djmvfb@mst.edu>
2020-04-12 10:28:17 -0600

    dummy proof config file for secrets def

--- thingiverse_dl/config.py
+++ thingiverse_dl/config.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import pathlib
+import sys
 import yaml
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class defaults(object):
@@ -11,10 +15,32 @@ class defaults(object):
 
 
 class secrets(object):
-    file = pathlib.Path(__file__).parent.parent.resolve() / 'secrets.yml'
+    yml_file = pathlib.Path(__file__).parent.parent.resolve() / 'secrets.yml'
+    # yml_dict = yaml.load(yml_file.read_text(),
+    #                      Loader=yaml.BaseLoader)
+    expected_config_keys = {'client_id', 'client_secret', 'app_token'}
+
 
 # load secrets.yml file and assign values to the secrets config object
-for key, value in yaml.load(
-        secrets.file.read_text(), Loader=yaml.BaseLoader
-).items():
-    setattr(secrets, key, value)
+try:
+    yaml_dict = yaml.load(secrets.yml_file.read_text(),
+                          Loader=yaml.BaseLoader)
+    missing_config_keys = secrets.expected_config_keys.difference(yaml_dict.keys())
+    if len(missing_config_keys) > 0:
+        raise KeyError()
+except FileNotFoundError as e:
+    # User did not create the `secrets.yml` file
+    logger.critical(f'Thingiverse secrets file not found at {secrets.yml_file}.\n'
+                    f'Please register your usage of this app with Thingiverse, '
+                    f'populate the "{secrets.yml_file.name}.template" file, '
+                    f'and rename it to "{secrets.yml_file.name}"')
+    sys.exit(1)
+except KeyError as e:
+    # User did not properly format the `secrets.yml` file
+    logger.critical(f'{secrets.yml_file} is incomplete.\n'
+                    f'\tExpected keys: {", ".join(secrets.expected_config_keys)}\n'
+                    f'\tMissing keys:  {", ".join(missing_config_keys)}')
+    sys.exit(1)
+else:
+    for key in secrets.expected_config_keys:
+        setattr(secrets, key, yaml_dict[key])
