caa63619618b73942e3f7dcbc2c05273af60b3ca
Doug McGeehan <djmvfb@mst.edu>
2020-04-17 08:00:17 -0600

    ugh, not working

--- thingiverse_dl/authentication/__init__.py
+++ thingiverse_dl/authentication/__init__.py
@@ -5,26 +5,39 @@ import requests
 
 from . import authorize
 from . import authenticate
+from .. import utilities
 
 logger = logging.getLogger(__name__)
 
 
-def required(function):
-    def wrapper(*args, **kwargs):
-        logger.info(f'{function.__name__}(args={args}, kwargs={kwargs})')
-        # Authorize this script to access a user's account
-        authorization_code = authorize.me()
+@utilities.singleton
+class AuthenticationManager(object):
+    def __init__(self, function):
+        self.function = function
+        self._session = None
 
-        # Obtain oath2 token for authentication
-        authentication_token = authenticate.me(code=authorization_code)
-
-        # Assign token to session
-        authenticated_session = requests.Session()
-        authenticated_session.headers.update({
-            'Authorization': f'Bearer {authentication_token}'
-        })
-
-        result = function(*args, **kwargs)
-        logger.info(f'Returned value: {result}')
+    def __call__(self, *args, **kwargs):
+        logger.info(f'{self.function.__name__}(args={args}, kwargs={kwargs})')
+        logger.info(self)
+        result = self.function(*args, **kwargs)
         return result
-    return wrapper
+
+    @property
+    def session(self):
+        if self._session is None:
+            # Authorize this script to access a user's account
+            authorization_code = authorize.me()
+
+            # Obtain oath2 token for authentication
+            authentication_token = authenticate.me(code=authorization_code)
+
+            # Assign token to session
+            authenticated_session = requests.Session()
+            authenticated_session.headers.update({
+                'Authorization': f'Bearer {authentication_token}'
+            })
+            self._session = authenticated_session
+        return self._session
+
+
+required = AuthenticationManager
---
+++ thingiverse_dl/utilities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import logging
+import functools
+
+logger = logging.getLogger(__name__)
+
+
+def singleton(cls):
+    """Make a class a Singleton class (only one instance)"""
+    @functools.wraps(cls)
+    def wrapper_singleton(*args, **kwargs):
+        if not wrapper_singleton.instance:
+            wrapper_singleton.instance = cls(*args, **kwargs)
+        return wrapper_singleton.instance
+    wrapper_singleton.instance = None
+    return wrapper_singleton
